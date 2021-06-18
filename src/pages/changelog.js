29dad3956cbdbb2f652ca72faac9bf997313bbc5
qfrery <qfrery@gmail.com>
2021-06-18 19:46:24 +0200

    changelog UI fine tuning

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -7,19 +7,19 @@ export default function Changelog({ data }) {
     return (
         <div className="relative py-16 bg-white overflow-hidden">
             {changelogs.map(changelog => (
-                <div className="relative px-4 sm:px-6 lg:px-8">
+                <div className="relative px-4 sm:px-6 lg:px-8 m-20">
                     <div className="text-lg max-w-prose mx-auto">
                         <h1>
-                            <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+                            <span className="block text-base text-indigo-600 font-semibold tracking-wide uppercase">
                                 {changelog.frontmatter.date}
                             </span>
-                            <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                            <span className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                                 {changelog.frontmatter.title}
                             </span>
                         </h1>
-                    </div>
-                    <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-                        <p>{changelog.excerpt}</p>
+                        <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+                            <p>{changelog.excerpt}</p>
+                        </div>
                     </div>
                 </div>
             ))}
