18632cb71ae1f0b2d22de6e09fd653aa8ef54aac
qfrery <qfrery@gmail.com>
2021-10-14 12:32:13 +0200

    hide changelog dates

--- src/pages/changelog.js
+++ src/pages/changelog.js
@@ -17,9 +17,9 @@ export default function Changelog({ data }) {
             <div className="relative px-6 lg:px-8 md:mx-20 mb-24 md:mb-32" key={changelog.id}>
               <div className="text-lg max-w-2xl mx-auto">
                 <h1>
-                  <span className="block text-sm md:text-base text-blue-600 font-semibold tracking-wide uppercase">
+                  {/* <span className="block text-sm md:text-base text-blue-600 font-semibold tracking-wide uppercase">
                     {changelog.frontmatter.date}
-                  </span>
+                  </span> */}
                   <span className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                     {changelog.frontmatter.title}
                   </span>
