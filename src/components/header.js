ee22be5867618485919363ed43f94cd0e33a1e2c
Quentin Frery <qfrery@gmail.com>
2022-08-12 11:29:39 +0200

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -129,11 +129,11 @@ export default function Example() {
                                 </Popover>
                             </Popover.Group>
                             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                                <a href="https://app.productfeedback.com" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                                <a href="https://quentinfr.typeform.com/to/YGGhQnA1" className="text-base font-medium text-gray-500 hover:text-gray-900">
                                     Sign In
                                 </a>
                                 <a
-                                    href="https://app.productfeedback.com/signup"
+                                    href="https://quentinfr.typeform.com/to/YGGhQnA1"
                                     className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
                                 >
                                     Get Started Free
